03971a28b530da9d618256c3dac9cf0c86b7f13c
James Coxon <175479+jamescoxon@users.noreply.github.com>
2018-05-02 06:57:39 +0100

    added get_pending to nano_parse.h

--- include/nano_parse.h
+++ include/nano_parse.h
@@ -11,5 +11,6 @@ int get_block_count();
 int get_work(nl_block_t *block);
 int get_head(nl_block_t *block);
 int get_block(char *block_hash, nl_block_t *block);
+int get_pending(char *account_address, char *pending_block);
 
 #endif
