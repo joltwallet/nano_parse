f3b04008f6e77ac9de0db47fd473d7107bbfc70d
Brian Pugh <bnp117@gmail.com>
2018-05-20 15:42:40 -0700

    Wifi Block Count unit-test

--- test/test_nano_parse_lws.c
+++ test/test_nano_parse_lws.c
@@ -1,4 +1,5 @@
 #include "unity.h"
+
 #if CONFIG_NANOPARSE_BUILD_W_LWS
 
 #include <stdio.h>
@@ -17,6 +18,8 @@ static const char *TAG = "[nanoparse_lws]";
 #include "esp_event_loop.h"
 #include "esp_wifi.h"
 #include "esp_wpa2.h"
+#include "nvs.h"
+#include "nvs_flash.h"
 #include "wifi_credentials.h"
 
 
@@ -57,6 +60,7 @@ void wifi_connect(){
         }
     };
     
+    nvs_flash_init();
     tcpip_adapter_init();
     
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
@@ -67,20 +71,23 @@ void wifi_connect(){
 
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
     ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
 }
-
 bool first_run = true;
 
 static void wifi_setup(){
     if ( first_run ){
         wifi_connect();
 		first_run = false;
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
 
 TEST_CASE("WiFi Block Count", TEST_TAG){
     wifi_setup();
-    TEST_IGNORE();
+    uint32_t count = nanoparse_lws_block_count();
+    printf("Server Block Count: %d", count);
+    TEST_ASSERT_MESSAGE(0 < count, "Block Count Zero");
 }
 
 #endif
