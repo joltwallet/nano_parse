aca4230204eb5dbd8212512aea5b9d9faf389369
Brian Pugh <bnp117@gmail.com>
2018-05-20 13:13:27 -0700

    Unit test framework

---
+++ test/test_lws.c
@@ -0,0 +1,29 @@
+#include "unity.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <esp_system.h>
+#include "sodium.h"
+
+#include "nano_lib.h"
+
+static const *char TEST_TAG = "[nano_lws]"
+
+TEST_CASE("Parse Open Block", TEST_TAG){
+    TEST_IGNORE()
+}
+TEST_CASE("Parse Send Block", TEST_TAG){
+    TEST_IGNORE()
+}
+TEST_CASE("Parse Receive Block", TEST_TAG){
+    TEST_IGNORE()
+}
+TEST_CASE("Parse Change Block", TEST_TAG){
+    TEST_IGNORE()
+}
+TEST_CASE("Parse State Block", TEST_TAG){
+    TEST_IGNORE()
+}
+
+
